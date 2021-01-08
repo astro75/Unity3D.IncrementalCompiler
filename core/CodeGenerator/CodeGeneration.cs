2cc662aa5fc376c3c86868ed1072ff67fde7b010
pauliusCib123 <35725813+pauliusCib123@users.noreply.github.com>
2021-01-08 18:33:24 +0200

    Generated static apply method from [Record] does not carry over type … (#22)
    * Generated static apply method from [Record] does not carry over type constraints
    
    * shorter code

--- core/CodeGenerator/CodeGeneration.cs
+++ core/CodeGenerator/CodeGeneration.cs
@@ -604,12 +604,15 @@ namespace IncrementalCompiler {
       TypeDeclarationSyntax cds, ICollection<FieldOrProp> props
     ) {
       var genericArgsStr = cds.TypeParameterList?.ToFullString().TrimEnd() ?? "";
+      var genericArgsWhereClauseStr = cds.ConstraintClauses.Count > 0
+        ? cds.ConstraintClauses.ToFullString()
+        : "";
       var funcParamsStr = JoinCommaSeparated(props, p => p.type + " " + p.identifier);
       var funcArgs = JoinCommaSeparated(props, p => p.identifier);
 
       return ParseClassMembers(
         $"public static {cds.Identifier.ValueText}{genericArgsStr} a{genericArgsStr}" +
-        $"({funcParamsStr}) => new {cds.Identifier.ValueText}{genericArgsStr}({funcArgs});"
+        $"({funcParamsStr}) {genericArgsWhereClauseStr} => new {cds.Identifier.ValueText}{genericArgsStr}({funcArgs});"
       );
     }
 
